cd9ff90c4b2442d05dbf25cf0cc8b6ce8335aa67
Hafiz Wildan Ahmed Siddiqui <160965922+hafizsiddiqui1211@users.noreply.github.com>
2025-03-09 07:33:01 +0500

    app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import instaloader
 insta = instaloader.Instaloader()
 acc = "username"  # replace "username" with real username.
 
-insta.download_profile(acc, profile_pic_only=False)
+insta.download_profile(acc, profile_pic_only=False) #replace "False" with "True" for only profile_picture.
